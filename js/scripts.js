198e60d7c6e9a36c112a9cda6775b8c092a85096
Katie Tuttle <katka.karolcikova@gmail.com>
2016-06-21 10:07:49 -0700

    Separate business logic and UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,71 @@
-var result="";
-function tableTenis(test) {
+var pocet = function(test) {
+  var testNumberArray = [];
   for(var i=1; i<=test; i++) {
-    if(i % 15 == 0) {
-      result = ("Ping Pong!");
-      $("#pingPongResult").append("<li>" + result+ "</li>");
-    } else if (i % 3 == 0) {
-      result = ("Ping");
-      $("#pingPongResult").append("<li>" + result+ "</li>");
-    } else if (i % 5 == 0) {
-      result = ("Pong");
-      $("#pingPongResult").append("<li>" + result+ "</li>");
-    } else {
-      $("#pingPongResult").append("<li>" + i + "</li>");
-    }
+    testNumberArray.push(i);
   }
-};
-$(function() {
+  return testNumberArray;
+}
+
+var result = function(inputArray) {
+  var resultArray = [];
+  inputArray.forEach(function(test) {
+    if (test % 15 == 0) {
+      resultArray.push('ping-pong !!!!');
+    } else if (test % 3 == 0) {
+      resultArray.push('Ping');
+    } else if (test % 5 == 0) {
+      resultArray.push('Pong');
+    } else {
+      resultArray.push(test);
+    }
+  });
+  return resultArray;
+}
+
+$(document).ready(function() {
   $("form#pingPongForm").submit(function(event) {
     var numberInput = parseInt($("input#numberInput").val());
-    $("#pingPongResult li").remove();
     event.preventDefault();
-    tableTenis(numberInput);
+  $("#pingPongResult li").remove();
+    var numberArray = pocet(numberInput);
+    var finalArray = result(numberArray);
+    $("#pingPongResult").append("<li>" + finalArray + "</li>");
   });
 });
+
+
+// var result = [];
+// function tableTenis(test) {
+//   debugger;
+//   // var result = [];
+//   for(var i=1; i<=test; i++) {
+//     if(i % 15 == 0) {
+//       result.push("Ping Pong!");
+//       // $("#pingPongResult").append("<li>" + result+ "</li>");
+//     } else if (i % 3 == 0) {
+//       result.push("Ping");
+//       // $("#pingPongResult").append("<li>" + result+ "</li>");
+//     } else if (i % 5 == 0) {
+//       result.push("Pong");
+//       // $("#pingPongResult").append("<li>" + result+ "</li>");
+//     } else {
+//       // $("#pingPongResult").append("<li>" + i + "</li>");
+//     }
+//     return result;
+//   }
+//
+// };
+// $(function() {
+//   $("form#pingPongForm").submit(function(event) {
+//     var numberInput = parseInt($("input#numberInput").val());
+//     for(var i=1; i<=result.length; i++) {
+//
+//     }
+//     console.log(numberInput)
+//     console.log(result);
+//     $("#pingPongResult li").remove();
+//
+//     event.preventDefault();
+//     tableTenis(numberInput);
+//   });
+// });
